3f9ffac01170e714f01fe7bf86da78c1768a7b55
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2025-11-10 10:45:33 +0000

    pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,26 +1,37 @@
-var titulo = document.querySelector(".titulo");
+var titulo =  document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+for (var i = 0; i < pacientes.length; i++){
 
-var tdImc = paciente.querySelector(".info-imc");
+    var paciente = pacientes[i];
 
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-if (peso <= 0 || peso >= 1000) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
-}
-
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var tdImc = paciente.querySelector(".info-imc");
+    
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+    
+    if (peso <= 0 || peso >= 1000){
+        console.log("Peso Inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+    }
+    
 if (altura <= 0 || altura >= 3.00){
     console.log("Altura inválida!");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida";
+}
+
+if (pesoEhValido && alturaEhValida){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
 }
