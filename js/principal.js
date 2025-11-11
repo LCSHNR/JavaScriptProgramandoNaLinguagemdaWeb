0595402342c0c3837ad53fb051a200f4af53ffc0
LUCAS HENRIQUE DE OLIVEIRA <henrique.oliveira03@escola.pr.gov.br>
2025-11-11 11:37:51 +0000

    form

--- js/principal.js
+++ js/principal.js
@@ -39,3 +39,41 @@ if (pesoEhValido && alturaEhValida){
 }
 
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function (event){
+    event.preventDefault();
+    var form = document.querySelector
+    ("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pracienteTr = document.createElement("tr");
+
+    var nomeTd = document = document.createElement ("td");
+    var pesoTd = document = document.createElement ("td");
+    var alturaTd = document = document.createElement ("td");
+    var gorduraTd = document = document.createElement ("td");
+    var imcTd = document = document.createElement ("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pracienteTr.append(nomeTd);
+    pracienteTr.appendChild(pesoTd);
+    pracienteTr.appendChild(alturaTd);
+    pracienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+
+
+
+)};
